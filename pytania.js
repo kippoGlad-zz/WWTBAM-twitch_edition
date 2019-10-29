ab40f1432fe1d98507f978183d4ddb6e9c3e9ee8
kipp0 <kippoweb@gmail.com>
2019-10-29 03:25:05 +0100

    optimized to one html file

---
+++ pytania.js
@@ -0,0 +1,57 @@
+let gameInfo = [
+    {
+        question: "Ile było w torbie?",
+        answers: ["Czy kilo bakłażana", "350", "Czy stufki", "Nic" ],
+        rightIndex: 2
+    },
+    {
+        question: "Jak nazywa się pies patiro?",
+        answers: ["Adi", "Odi", "Olek", "Bolek"],
+        rightIndex:  1
+    },
+    {
+        question: "Kto jest właścicielem twitcha?",
+        answers: ["Google", "Facebook", "Amazon", "Twitter"],
+        rightIndex: 2
+    },
+    {
+        question: "Która z nich NIE streamowała na twitchu?",
+        answers: ["Sasha Grey", "Mia Malkova", "Mia Khalifa", "Piper Perri"],
+        rightIndex: 1
+    },
+    {
+        question: "Która z nich NIE streamowała na twitchu?",
+        answers: ["Sasha Grey", "Mia Malkova", "Mia Khalifa", "Piper Perri"],
+        rightIndex: 2
+    },
+    {
+        question: "Która z nich NIE streamowała na twitchu?",
+        answers: ["Sasha Grey", "Mia Malkova", "Mia Khalifa", "Piper Perri"],
+        rightIndex: 3
+    },
+    {
+        question: "Która z nich NIE streamowała na twitchu?",
+        answers: ["Sasha Grey", "Mia Malkova", "Mia Khalifa", "Piper Perri"],
+        rightIndex: 3
+    },
+    {
+        question: "Która z nich NIE streamowała na twitchu?",
+        answers: ["Sasha Grey", "Mia Malkova", "Mia Khalifa", "Piper Perri"],
+        rightIndex: 2
+    },
+    {
+        question: "Która z nich NIE streamowała na twitchu?",
+        answers: ["Sasha Grey", "Mia Malkova", "Mia Khalifa", "Piper Perri"],
+        rightIndex: 1
+    },
+    {
+        question: "Która z nich NIE streamowała na twitchu?",
+        answers: ["Sasha Grey", "Mia Malkova", "Mia Khalifa", "Piper Perri"],
+        rightIndex: 4
+    },
+    {
+        question: "Która z nich NIE streamowała na twitchu?",
+        answers: ["Sasha Grey", "Mia Malkova", "Mia Khalifa", "Piper Perri"],
+        rightIndex: 4
+    }
+]
